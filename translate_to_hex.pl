5d777dc22bd510c5bea96fc1e28beafd2fd1093c
kur <kur@niboch-cluster.(none)>
2013-05-16 01:29:57 -0400

    subroutines merged in one file

--- translate_to_hex.pl
+++ translate_to_hex.pl
@@ -14,7 +14,7 @@ while (!eof $csfasta_file){
     my $string = <$csfasta_file>;
 
     print $header;
-    print join " ", translate_string($string);
+    print join " ", join_regular_hex(translate_string($string));
     print "\n";
 }
 
@@ -44,4 +44,102 @@ sub translate_string {
 
     }
     return @result_string;
+}
+
+sub join_regular_hex {
+    my %g_block = (
+        'G1' , 'H65_H88',
+        'G3' , 'H25_M88',
+        'G2' , 'H66_H80_H89', 
+        'G4' , 'H26_M89',
+        'G5' , 'M53_A13', 
+        'G6' , 'H67_H81', 
+        'G7' , 'M33_M46', 
+        'G11' ,'H33_H85', 
+        'G9' , 'H11_H46',
+        'G10' , 'H14_M44', 
+        'G8' , 'H10_H45',
+    );
+    my @string = @_;
+    my @translated_string = ();
+    
+    my %str_p;
+    $str_p{'fr_l'} = '';
+    $str_p{'fr_N'} = 0;
+    $str_p{'cur_N'} = 0;
+
+    for (my $i = 0; $i < scalar (@string);$i++){
+        
+        #if unknown hexamer
+        if ($string[$i] =~ m/^\d{6}$/) {
+            
+            #push collected string
+            if ($str_p{'fr_l'} ne ''){
+                my $out_string = $str_p{'fr_l'}.$str_p{'fr_N'}."-".$str_p{'cur_N'};
+                push (@translated_string, $out_string);
+                $str_p{'fr_l'} = '';
+                $str_p{'fr_N'} = 0;
+                $str_p{'cur_N'} = 0;
+            }
+            
+            #push current hexamer
+            push (@translated_string, $string[$i]);
+        }
+        
+        #if known hexamer, but new in chain
+        elsif ($str_p{'fr_l'} eq ''){
+            $string[$i] =~ m/^([GHMA])(\d{1,2})/;
+            $str_p{'fr_l'} = $1;
+            $str_p{'fr_N'} = $2;
+            $str_p{'cur_N'} = $2;
+            #check if last cycle
+            if ($i == scalar(@string) -1){
+                my $out_string = $str_p{'fr_l'}.$str_p{'fr_N'}."-".$str_p{'cur_N'};
+                push (@translated_string, $out_string);
+             }   
+        }
+        #if new hexamer and chain exists
+        elsif ($str_p{'fr_l'} ne ''){
+            $string[$i] =~ m/^([GHMA])(\d{1,2})/;
+            my $f1 = $1;
+            my $f2 = $2;
+            #continue chain
+            if ( $f1 eq 'G'){
+                $f1 = $str_p{'fr_l'};
+                $f2 = $str_p{'cur_N'} + 1;
+            }
+            if ($str_p{'fr_l'} eq 'G' and $f1 ne 'G'){
+                $str_p{'fr_l'} = $f1;
+                $str_p{'fr_N'} = $f2 - ($str_p{'cur_N'}-$str_p{'fr_N'}+1);
+                $str_p{'cur_N'} =$f2 -1;
+            }
+
+
+
+            if ($f1 eq $str_p{'fr_l'} and $f2 == 1+$str_p{'cur_N'}){
+                $str_p{'cur_N'} = $f2;
+
+                if ($i == scalar(@string) -1){
+                    my $out_string = $str_p{'fr_l'}.$str_p{'fr_N'}."-".$str_p{'cur_N'};
+                    push (@translated_string, $out_string);
+                }   
+            }
+            #stop chain
+            else {
+                my $out_string = $str_p{'fr_l'}.$str_p{'fr_N'}."-".$str_p{'cur_N'};
+                push (@translated_string, $out_string);
+                $str_p{'fr_l'} = $1;
+                $str_p{'fr_N'} = $2;
+                $str_p{'cur_N'} = $2;
+                if ($i == scalar(@string) -1){
+                    my $out_string = $str_p{'fr_l'}.$str_p{'fr_N'}."-".$str_p{'cur_N'};
+                    push (@translated_string, $out_string);
+                }   
+            }
+        }
+    }
+
+
+
+    return @translated_string
 }
